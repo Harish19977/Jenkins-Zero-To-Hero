ee2785d9a7f466a2f41792b7a64a3a49c9eb871b
Harish19977 <hariymore143@gmail.com>
2026-01-04 10:39:49 +0530

    Create HelloWorld.java

---
+++ java-hello-world4/src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World from java-hello-world4 CI/CD!");
+        while(true) {
+            try {
+                Thread.sleep(10000);
+            } catch (Exception e) {}
+        }
+    }
+}
